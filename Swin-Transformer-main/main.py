166db2dee385646c84ff8e7463adfc605f8ad928
cihaneraslan <54939925+cihaneraslan@users.noreply.github.com>
2021-11-01 19:36:32 +0400

    Update main.py

--- Swin-Transformer-main/main.py
+++ Swin-Transformer-main/main.py
@@ -149,7 +149,7 @@ def main(config):
 
 def train_one_epoch(config, model, criterion, data_loader, optimizer, epoch, mixup_fn, lr_scheduler):
     try:
-        print('bnnbnmnnmnn')
+        print('bnnbnmndnmnn')
     except SecurityError:
     pass
         
